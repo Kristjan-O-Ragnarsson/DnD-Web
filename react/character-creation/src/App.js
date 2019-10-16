78b3bae462fd71bf12a59486a687c5a1e1571e58
Daníel Þór Þórisson <danielthorr@gmail.com>
2019-10-16 14:24:31 +0000

    Testing axios to fetch data
    Added axios with npm for use with react. It fetches date and simplifies error checking. Less work for us and more robust error checking. Win-win situation. - As far as I understand, that is

--- react/character-creation/src/App.js
+++ react/character-creation/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+// Fetching data with react hooks: https://www.robinwieruch.de/react-hooks-fetch-data
 
 function App() {
   return (
