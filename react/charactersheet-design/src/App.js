08e6ac0b3ff5f1f22eb61427d90a353f7e59aaad
Daníel Þór Þórisson <danielthorr@gmail.com>
2019-10-28 23:14:10 +0000

    Trying out some formatting
    Edit: Fixed diff, just needed to pull missing files from remote
    Git --amend note: Check diff to make sure I'm not changing anything I shouldn't

--- react/charactersheet-design/src/App.js
+++ react/charactersheet-design/src/App.js
@@ -1,19 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Form } from 'semantic-ui-react';
- 
+import { Form, Grid, Divider, Segment } from 'semantic-ui-react';
+
+function BaseInfo() {
+  return(
+    <React.Fragment>
+      <Form.Group>
+        <Form.Input label="Character name" width={6} />
+        <Form.Input label="Player name" width={6} />
+      </Form.Group>
+      <Form.Group>
+        <Form.Input label="Class" />
+        <Form.Input label="Race" />
+        <Form.Input label="Level" />
+      </Form.Group>
+    </React.Fragment>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <Form>
-        <Form.Group>
-          <Form.Input label="Character name" width={6} />
-          <Form.Input label="Class" width={2} />
-          <Form.Input label="Race" width={2} />
-          <Form.Input label="Level" width={2} />
-          <Form.Input label="Player name" width={4} />
-        </Form.Group>
+
+        < BaseInfo />
+
+        <Segment>
+          <Grid columns={2} relaxed="very">
+            <Grid.Column>
+              <Form.Group inline>
+                <Form.Input label="Strength" width={2} />
+                <p> + (Mod from race) + (any other mod) = (Final sum [Modified])</p>
+              </Form.Group>
+              <Form.Field inline={true} width={6}>
+                <label>Player name</label>
+                <input type="text" />
+                <label>label number dos</label>
+              </Form.Field>
+            </Grid.Column>
+            <Grid.Column>
+              <Form.Input label="Class" />
+              <Form.Input label="Race" />
+              <Form.Input label="Level" />
+            </Grid.Column>
+          </Grid>
+          <Divider vertical></Divider>
+        </Segment>
       </Form>
     
     </div>
