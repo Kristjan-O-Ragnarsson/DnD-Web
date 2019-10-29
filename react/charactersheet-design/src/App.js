b39859dab221248a9ec158814f26c619a1084efb
Daníel Þór Þórisson <danielthorr@gmail.com>
2019-10-29 23:53:47 +0000

    Form is coming along

--- react/charactersheet-design/src/App.js
+++ react/charactersheet-design/src/App.js
@@ -1,54 +1,122 @@
 import React from 'react';
 import './App.css';
-import { Form, Grid, Divider, Segment } from 'semantic-ui-react';
+import { Container, Form, Grid, Divider, Segment, Header, Table, Popup } from 'semantic-ui-react';
 
-function BaseInfo() {
-  return(
-    <React.Fragment>
-      <Form.Group>
-        <Form.Input label="Character name" width={6} />
-        <Form.Input label="Player name" width={6} />
-      </Form.Group>
-      <Form.Group>
-        <Form.Input label="Class" />
-        <Form.Input label="Race" />
-        <Form.Input label="Level" />
-      </Form.Group>
-    </React.Fragment>
-  );
+const formFields = {
+  group1: [
+    <Form.Input label="Character name" width={6} />,
+    <Form.Input label="Player name" width={6} />
+  ],
+  group2: [
+    <Form.Input label="Class" />,
+    <Form.Input label="Race" />,
+    <Form.Input label="Level" />
+  ],
+  group3: [
+    <Form.Input label="HP" width={2} />,
+    <Form.Input label="AC" width={2} />,
+    <Form.Input label="Initiative" width={2} />,
+    <Form.Input label="Speed" width={2}/>
+  ],
+  groupSample: [
+    <Form.Input label="" />
+  ]
+}
+
+class AbilityTable extends React.Component {
+  constructor(props){
+    super(props);
+    this.abilities = ["str", "dex", "con", "int", "wis", "char"];
+    this.headers = ["Ability", "Base", "Race", "Other", "Sum"];
+    this.state = {
+      abilities: [
+        { name: "Strength", id: "str", base: 0, raceMod: 0, otherMod: 0},
+        { name: "Dexterity", id: "dex", base: 0, raceMod: 0, otherMod: 0},
+        { name: "Constition", id: "con", base: 0, raceMod: 0, otherMod: 0},
+        { name: "Intelligence", id: "int", base: 0, raceMod: 0, otherMod: 0},
+        { name: "Wisdom", id: "wis", base: 0, raceMod: 0, otherMod: 0},
+        { name: "Charisma", id: "char", base: 0, raceMod: 0, otherMod: 0},
+      ],
+      elems: []
+    };
+  }
+
+  updateAbilityScore(ability) {
+    return ability.base + ability.raceMod + ability.otherMod;
+  }
+
+  render() {
+
+    let headers = [];
+    this.headers.forEach(header => {
+      headers.push(<Table.HeaderCell>{header}</Table.HeaderCell>);
+    });
+    this.state.elems.push(
+      <Table.Row>{headers}</Table.Row>
+    );
+
+    this.state.abilities.forEach((ability, i) => {
+      this.state.elems.push(
+        <Table.Row>
+          <Table.Cell>{ability.name}</Table.Cell>
+          <Table.Cell>
+            <Form.Input 
+              id={ability.id} 
+              fluid 
+              type="number" 
+              value={ability.base}
+            />
+          </Table.Cell>
+          <Table.Cell>{ability.raceMod}</Table.Cell>
+          <Table.Cell>{ability.otherMod}</Table.Cell>
+          <Table.Cell>{this.updateAbilityScore(ability)}</Table.Cell>
+        </Table.Row>
+      );
+    });
+
+    return (
+      <Table unstackable striped textAlign="center" >
+        {this.state.elems}
+
+      </Table>
+    );
+  }
 }
 
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
       <Form>
 
-        < BaseInfo />
+        <Divider horizontal>
+          <Header as="h3" />
+
+        </Divider>
+
+        <Form.Group>
+          {formFields.group1}
+        </Form.Group>
 
         <Segment>
-          <Grid columns={2} relaxed="very">
+
+          <Form.Group>
+            {formFields.group2}
+          </Form.Group>
+
+          <Grid columns={2} stackable>
             <Grid.Column>
-              <Form.Group inline>
-                <Form.Input label="Strength" width={2} />
-                <p> + (Mod from race) + (any other mod) = (Final sum [Modified])</p>
-              </Form.Group>
-              <Form.Field inline={true} width={6}>
-                <label>Player name</label>
-                <input type="text" />
-                <label>label number dos</label>
-              </Form.Field>
+              <AbilityTable />
             </Grid.Column>
             <Grid.Column>
-              <Form.Input label="Class" />
-              <Form.Input label="Race" />
-              <Form.Input label="Level" />
+              <Form.Group>
+                {formFields.group3}
+              </Form.Group>
             </Grid.Column>
           </Grid>
-          <Divider vertical></Divider>
         </Segment>
       </Form>
     
-    </div>
+    </Container>
   );
 }
 
