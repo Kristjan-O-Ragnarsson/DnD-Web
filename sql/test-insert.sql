df41cb57acd028877911f3a82c49c2b81de1c161
Kristjan-O-Ragnarsson <kristjan@ragnarsson.im>
2019-10-30 14:40:27 +0000

    sql test data

---
+++ sql/test-insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO `alignment` (`name`)
+VALUES ("Lawfull Evil")
+
+INSERT INTO `users` (`nickname`)
+VALUES ("Kristcx")
+
+INSERT INTO `race` (`name`)
+VALUES ("Elf")
+
+INSERT INTO `character` (`name`, `user_id`, `race_id`, `alignment`, `xp`)
+VALUES ("Kníður", 1, 1, 1, 12)
